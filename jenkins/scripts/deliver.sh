e557155a768180101ec158abb867c70a3953ca66
George Darras <georgios.darras@netcompany.com>
2024-11-15 17:37:58 +0200

    maven

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -20,6 +20,11 @@ set -x
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 set +x
 
+echo 'path'
+set -x
+pwd
+set +x
+
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
